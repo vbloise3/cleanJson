75da1586d8c061a901b4c90db0c2615eda26a1f5
Vince Bloise <vbloise3@gmail.com>
2020-03-20 13:59:16 -0400

    first commit

---
+++ cleanJson.py
@@ -0,0 +1,18 @@
+import json
+import re
+fread = open("ML_Practice_Exam_Questions_ml.json", "r")
+
+fwrite = open("nullLess.json","w")
+
+contents = fread.read()
+
+cleanNewContents = contents.replace("–", "-").replace('—', '-').replace('…', '...').replace('‚', ',').replace('„', '"').replace('‘', "'").replace("’", "'").replace('›', '>').replace('©', '').replace('«', '<<').replace('®', '').replace('¯', '').replace('°', ' degrees').replace("´", "'").replace('»', '>>').replace('×', 'x').replace('÷', '/').replace('¼', '1/4').replace('½', '1/2').replace('¾', '3/4').replace('“', '"').replace('”', '"').replace('ˆ', '^').replace('€', ' pounds').replace('™', ' trade mark')
+
+print("contents lenth: ", len(contents))
+print("cleanContents length: ", len(cleanNewContents))
+
+nullCount = contents.count('–')
+print("null count: ", nullCount)
+
+fwrite.write(cleanNewContents)
+fwrite.close()
